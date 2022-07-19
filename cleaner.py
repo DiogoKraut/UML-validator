7ae47cbd8ebc4bac80131e317e71f10bee790b63
Jean Carlo Sanchuki Filho <jeancsf@mppr.mp.br>
2022-07-19 12:50:29 -0300

    feat: removing xmi prefix

--- cleaner.py
+++ cleaner.py
@@ -2,23 +2,29 @@ import shutil
 import os
 
 def cleaner():
-    replace_list = ["\"xmi:", "\"xmlns:", "\"uml:"]
+    replace_dict = {"xmi:type":"xmitype"}
+    remove_list = ["xmlns:", "uml:", "xmi:"]
     shutil.copy2('./data.json', './temp.txt')
     newJson = open("out.json", "w")
     with open("temp.txt", "r") as filej:
         for line in filej:
             count=0
             spc=0
+            #Pula as linhas em branco em busca do início dos nomes dos atributos.
             for char in line:
                 if(char != " "):
                     count+=1
                 spc+=1
                 if(count == 2):
+                    #Troca os caracteres iniciais que deseja ser removido.
                     if (char == '@'):
                         line = line.replace("@", "", 1)
-                    for text in replace_list:
-                        if(line.startswith(text, spc-2)):
-                            line = line.replace(text, text[:-1])
+                    for key in replace_dict:
+                        if(line.startswith(key, spc-1)):
+                            line = line.replace(key, replace_dict[key])
+                    for text in remove_list:
+                        if(line.startswith(text, spc-1)):
+                            line = line.replace(text, "", 1)
             newJson.write(line)
         filej.close()
     os.remove(filej.name)
