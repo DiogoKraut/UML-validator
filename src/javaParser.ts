2dc6f532544672d9e0d396d11be71b886304445c
jeansanch <jean.sanchuki@gmail.com>
2022-08-16 18:11:58 -0300

    fix: ignoring operation data and fixing op. name

--- src/javaParser.ts
+++ src/javaParser.ts
@@ -7,13 +7,31 @@ export function javaClassParser(filePath: string) : MetaClass {
   const source = fs.readFileSync(filePath, 'utf-8');
   const lines = source.split(/\r?\n/);
   const newClass = new MetaClass();
+  let wasOp = false;
+  let count = 0;
   for (const line of lines) {
     let newLine = ltrim(line);
-    const words = newLine.split(/[\s;(), ]+/);
+    const words = newLine.split(/[\s;, ]+/);
     let flag = newLine[newLine.length-1] == ";";
+    let flagFuncName = false;
     let flagClass = false;
 
+    // check if it has return type
+    let compLine = line.replace(/[(){}]+/, '');
+    if (compLine != newLine)
+      flagFuncName = true;
+
     // if it is import, empty line or braces, skip
+    if (wasOp){
+      if(newLine[newLine.length-1] == '{' || words[0] == '{')
+        count ++;
+      if(newLine[newLine.length-1] == '}' || words[0] == '}'){
+        count --;
+        if (count == 0)
+          wasOp = false;
+      }
+      continue;
+    }
     if (words[0] == 'import' || words[0] == '}' || words[0] == '' || words[0] == '{') continue;
     if (words[0] == 'class'){
       newClass.name = words[1];
@@ -27,7 +45,8 @@ export function javaClassParser(filePath: string) : MetaClass {
       if (flag) {
         parseAttribute(words, newClass);
       } else {
-        parseOperation(words, newClass);
+        wasOp = true;
+        parseOperation(words, newClass, flagFuncName);
       }
     }
   }
@@ -44,10 +63,12 @@ function parseAttribute(words: string[], newClass: MetaClass) {
   newClass.ownedAttribute.push(attribute);
 }
 
-function parseOperation(words: string[], newClass: MetaClass) {
+function parseOperation(words: string[], newClass: MetaClass, flag: boolean) {
   const op = new MetaOperation();
   op.visibility = words[0];
-  op.name = words[1];
+  op.name = flag ? words[2] : words[1];
+  op.name = op.name.replace(/[(){]+/, '');
+  op.returnType = flag ? words[1] : '';
   newClass.ownedOperation.push(op);
 }
 
