d690a3c4923e8f97d6192c1bac02a6ca4661aeda
jeansanch <43691588+jeansanch@users.noreply.github.com>
2022-08-17 20:14:34 -0300

    Feat/parse class attributes operations (#8)
    * feat: attribute and operation parsing
    
    * fix: ignoring operation data and fixing op. name
    
    * chore: added comments
    
    * fix: comparing wrong string
    
    * chore: trim function renamed added comment on it

--- src/entities/meta-operation.entity.ts
+++ src/entities/meta-operation.entity.ts
@@ -2,6 +2,7 @@ import { MetaParameter } from "./meta-parameter.entity";
 
 export class MetaOperation {
   "name": string;
+  "returnType": string;
   "visibility": string;
   "id": string;
   "xmitype": string;
--- src/javaParser.ts
+++ src/javaParser.ts
@@ -7,30 +7,58 @@ export function javaClassParser(filePath: string) : MetaClass {
   const source = fs.readFileSync(filePath, 'utf-8');
   const lines = source.split(/\r?\n/);
   const newClass = new MetaClass();
-
+  let wasOp = false;
+  let count = 0;
   for (const line of lines) {
-    const words = line.split(/[\s;(),]+/);
-    // if is import or empty line, skip
-    if (words[0] == 'import' || words[0] == '') continue;
+    let newLine = trimSpaces(line);
+    const words = newLine.split(/[\s;, ]+/);
+    let flagAtt = newLine[newLine.length-1] == ";";
+    let flagFuncName = false;
+    let flagClass = false;
+
+    // check if it has return type
+    let compLine = newLine.replace(/[(){}]+/, '');
+    if (compLine != newLine)
+      flagFuncName = true;
+
+    // check if it is inside an operation and ignore
+    if (wasOp){
+      if(newLine[newLine.length-1] == '{' || words[0] == '{')
+        count ++;
+      if(newLine[newLine.length-1] == '}' || words[0] == '}'){
+        count --;
+        if (count == 0)
+          wasOp = false;
+      }
+      continue;
+    }
+
+    // if it is import, empty line or braces, skip
+    if (words[0] == 'import' || words[0] == '}' || words[0] == '' || words[0] == '{') continue;
     
-    let i;
-    for (i = 0; i < words.length; i++) {
-      if (words[1] == 'class' || words[0] == 'class') {
-        newClass.name = words[2];  //todo fix words[0] case
+    if (words[0] == 'class'){
+      newClass.name = words[1];
+      flagClass = true;
+    }
+    if (words[1] == 'class'){
+      newClass.name = words[2];
+      flagClass = true;
+    }
+    if (!flagClass){
+      if (flagAtt) {
+        parseAttribute(words, newClass);
+      } else {
+        wasOp = true;
+        parseOperation(words, newClass, flagFuncName);
       }
     }
-    if (words[words.length] == ';') {
-      parseAttribute(words, newClass);
-    } else {
-      parseOperation(words, newClass);
-    }
-
   }
   console.log(JSON.stringify(newClass, null, 2));
   return newClass;
 }
 
 function parseAttribute(words: string[], newClass: MetaClass) {
+ 
   const attribute = new MetaAttribute();
   attribute.visibility = words[0];
   attribute.type = words[1];
@@ -38,9 +66,17 @@ function parseAttribute(words: string[], newClass: MetaClass) {
   newClass.ownedAttribute.push(attribute);
 }
 
-function parseOperation(words: string[], newClass: MetaClass) {
+function parseOperation(words: string[], newClass: MetaClass, flag: boolean) {
   const op = new MetaOperation();
   op.visibility = words[0];
-  op.name = words[2];
+  op.name = flag ? words[2] : words[1];
+  op.name = op.name.replace(/[(){]+/, '');
+  op.returnType = flag ? words[1] : '';
   newClass.ownedOperation.push(op);
+}
+
+// remove spaces from the beggining of a line
+function trimSpaces(line: string) {
+  if(!line) return line;
+  return line.replace(/^\s+/g, '');
 }
