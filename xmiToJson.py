5a39674d32bcfa5b5ab41018e5cc2f8718a2f046
Diogo Kraut <diogo.kraut@hotmail.com>
2022-09-11 16:38:26 -0300

    feat: class diagram attribute match (#10)
    * feat: attribute and operation parsing
    
    * fix: ignoring operation data and fixing op. name
    
    * chore: added comments
    
    * fix: comparing wrong string
    
    * chore: trim function renamed added comment on it
    
    * feat/xmi attribute id parsing
    
    * test/test commit (please ignore)
    
    Co-authored-by: jeansanch <jean.sanchuki@gmail.com>

--- xmiToJson.py
+++ xmiToJson.py
@@ -1,10 +1,20 @@
 import json
+import re
 from sys import argv
 import xmltodict
 
 input = argv[1]
 with open(input) as xml_file:
-     
+    for line in xml_file:
+        if(re.search('(?<=<)packagedElement'))
+        attType = re.search('(?<=name=")([A-z])+', line)
+        attId = re.search('(?<=xmi:id=")([A-z_0-9-])+', line)
+        attClass = re.search('(?<=xmi:type=")([A-z_:0-9-])+', line)
+        if(attType and attId):
+            if(attClass and attClass.group() == "uml:Class"):
+                print(attType.group() + ", " + attId.group() + ", " + attClass.group())
+    xml_file.close()
+with open(input) as xml_file:
     data_dict = xmltodict.parse(xml_file.read())
     xml_file.close()
      
