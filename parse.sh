2826fd6910be62c26f7aa551c60fd60b41a504ae
Diogo Kraut <diogo.kraut@hotmail.com>
2023-01-23 14:46:47 -0300

    initial commit

--- parse.sh
+++ parse.sh
@@ -1,17 +1,22 @@
-while getopts i:o: flag
-do
-  case "${flag}" in
-    i) input=${OPTARG}
-      ;;
-    o) output=${OPTARG}
-      ;;
-  esac
-done
+# while getopts i:o: flag
+# do
+#   case "${flag}" in
+#     i) input=${OPTARG}
+#       ;;
+#     o) output=${OPTARG}
+#       ;;
+#   esac
+# done
 
-if [ -z ${input} ] || [ -z ${output} ]
-then
-  echo "Usage: parse.sh -i <input XMI> -o <output JSON>\n"
-else
-  python3 xmiToJson.py ${input};
-  python3 cleaner.py ${output};
-fi
+# if [ -z ${input} ] || [ -z ${output} ]
+# then
+#   echo "Usage: parse.sh -i <input XMI> -o <output JSON>\n"
+# else
+cd tests
+for d in */ ; do
+  echo $d;
+  echo "${$d/*.xmi}"
+  python3 ../xmiToJson.py $d/*.xmi;
+  python3 ../cleaner.py $d/diagram.json;
+done
+# fi
